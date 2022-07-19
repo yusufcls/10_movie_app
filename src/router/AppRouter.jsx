22b445dda4837587ecf191143efd16d13f9adf62
Yusuf C <yusufcfly@gmail.com>
2022-07-19 20:05:51 +0200

    phase_02

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,10 +1,22 @@
 import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Navbar from '../components/Navbar';
+import Login from '../pages/Login';
+import Main from '../pages/Main';
+import MovieDetail from '../pages/MovieDetail';
+import Register from '../pages/Register';
 
 const AppRouter = () => {
   return (
-    <div>
-      <p>Router</p>
-    </div>
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/details/:id" element={<MovieDetail />} />
+      </Routes>
+    </BrowserRouter>
   );
 };
 
